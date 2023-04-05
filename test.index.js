a0869bf2489119e7ed66485b218a0e96d3f1b329
Dan Prudhomme <dannyprud@yahoo.com>
2023-04-05 11:41:56 -0500

    Update test file and log

--- test.index.js
+++ test.index.js
@@ -1,5 +1,5 @@
 const LoggerLight = require("./LoggerLight");
 const logger = new LoggerLight("Test Dir", "Test Logs");
 
-logger.log({ test: "test" });
+logger.log({ test: "test" }, "ERROR");
 logger.log("test");
